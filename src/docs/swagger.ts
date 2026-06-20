b41ede1d3fb2292ec5d61027613fc6ba84fa9aca
Adithya Raihan Pratama <144696680+Adithyaraihan@users.noreply.github.com>
2026-06-20 20:10:06 +0700

    perbaikan port in swagger

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -10,7 +10,7 @@ const doc = {
     },
     servers: [
         {
-            url: 'http://localhost:3000/api',
+            url: 'http://localhost:5000/api',
             description: 'Localhost'
         },
         {
